73f00c5dbe6d4133df47779044dc33a958be8c75
Erik-joh <erik.johannesson0@gmail.com>
2019-10-23 11:31:30 +0200

    added getAuthor to functions

--- functions.php
+++ functions.php
@@ -2,3 +2,11 @@
 declare(strict_types=1);
 // This is the file where you can keep all your functions. Remember to NOT
 // execute/run any functions in this file. Keep it dumb.
+
+function getAuthor(array $article, array $authors):string{
+    foreach ($authors as $author){
+        if($article['authorId'] === $author['id']){
+            return $author['name'];
+        }
+    }
+}
--- index.php
+++ index.php
@@ -2,14 +2,30 @@
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
+require __DIR__.'/functions.php';
+require __DIR__.'/data.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title></title>
+        <link href="style.css" rel="stylesheet">
+        <title>Plain News</title>
     </head>
     <body>
+
+        <?php foreach($articles as $article): ?>
+            <div class="article">
+                <h2><?php echo $article['title'] ?></h2>
+                <p><?php echo $article['content'] ?></p>
+                <div class ="articleFooter">
+                    <p><?php echo $article['date'] ?></p>
+                    <p><?php echo getAuthor($article,$authors) ?></p>
+                    <p><?php echo $article['likeCounter'] ?></p>
+                </div>
+            </div>
+        <?php endforeach; ?>
+
     </body>
 </html>
