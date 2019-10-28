736e7fb3d8ac3ce5a9663b967041133adb60454b
Erik-joh <erik.johannesson0@gmail.com>
2019-10-28 11:51:29 +0100

    Added a database instead of arrays

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 // This is the file where you can keep your HTML markup. We should always try to
 // keep us much logic out of the HTML as possible. Put the PHP logic in the top
 // of the files containing HTML or even better; in another PHP file altogether.
+
 require __DIR__.'/functions.php';
-require __DIR__.'/data.php';
-$articles = sortArticles($articles);
+
+$fileName = __DIR__ . "/plainNews.db";
+$db = new PDO("sqlite:$fileName");
+$query = $db->query('SELECT * FROM articles INNER JOIN authors ON articles.author_id = authors.id ORDER BY date DESC');
+$articles = $query->fetchAll(PDO::FETCH_ASSOC);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,13 +21,13 @@ $articles = sortArticles($articles);
     </head>
     <body>
         <article class="midFeed">
-            <?php foreach($articles as $article): ?>
+            <?php foreach($articles as $article):?>
                 <div class="article">
                     <h2><?php echo $article['title'] ?></h2>
                     <div class ="ADLContainer">
-                        <p>by <?php echo getAuthor($article,$authors) ?></p>
+                        <p>by <?php echo $article['name'] ?></p>
                         <p>Published: <?php echo getDMY($article['date']) ?></p>
-                        <p>Likes: <?php echo $article['likeCounter'] ?></p>
+                        <p>Likes: <?php echo $article['likes'] ?></p>
                     </div>
                     <img src="/img/<?php echo $article['image'] ?>" class="articleImage">
                     <p><?php echo $article['content'] ?></p>
